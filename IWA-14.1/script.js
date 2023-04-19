101ee88c6bc4cf2a76aee4e6b943ac65292ba728
Hennie Scheepers <hennie.104@hotmail.com>
2023-04-19 11:02:29 +0200

    added comments to help explain

--- IWA-14.1/script.js
+++ IWA-14.1/script.js
@@ -6,14 +6,17 @@ hobby = 'Coding';
  * @param {string} parameter
 */
 
-//added .log after console
+
+//                () -> parameter
 const logTwice =  (parameter) => {
+  //console() -> console.log
   console.log(parameter)
   console.log(parameter)
 }
 
 //Changed hobby to hobbyLog as hobby is already a variable
 function hobbyLog () {
+  //                 name -> firstName
   logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
 }
 
--- IWA-14.2/script.js
+++ IWA-14.2/script.js
@@ -1,12 +1,18 @@
 // script.js
 
+//function -> 
+//          a,b -> (a.b)
 const add = (a, b) =>  a + b
 
+//                a,b -> (a.b)
 const multiply = (a, b) =>  a * b 
 
 function internal() {
+	//                     this.a -> this.internal.a
 	const added = this.add(this.internal.a, this.internal.b)
+	// created multiplied variable
 	const multiplied = this.multiply(added, this.internal.c)
+	//return multiplied
 	return console.log(multiplied)
 }
 
